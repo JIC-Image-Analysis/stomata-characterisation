b20b48b9cd82727c301ca25fca9507785d5749ea
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-05-29 09:41:33 +0100

    Cleaned up find_opening.py.

--- scripts/find_opening.py
+++ scripts/find_opening.py
@@ -31,17 +31,27 @@ def box_of_interest(image_collection, stomata_id):
     box = ellipse_of_interest(image_collection, first, region)
     return box
 
+def minor_line(box):
+    """Return the two points representing the minor line from the ellipse box."""
+    p1, p2, p3, p4 = quadrant_lines_from_box(box)
+    return p1, p2
+
+def opening_line(image_collection, series_id, box):
+    """Return the two point representing the stomata opening line."""
+    minor_pt1, minor_pt2 = minor_line(box)
+    cut1, cut2 = opening_points(image_collection, series_id, box)
+    line = minor_pt2 - minor_pt1
+    m = line.magnitude
+    inner_pt1 = minor_pt1 + line * (cut1.x / line.magnitude)
+    inner_pt2 = minor_pt1 + line * (cut2.x / line.magnitude)
+    return inner_pt1, inner_pt2
+
 def find_opening(image_collection, stomata_id, timepoint):
     """Find the stomata opening write out image."""
     series_id = series_identifier(stomata_id, timepoint)
     box = box_of_interest(image_collection, stomata_id)
-    p1, p2, p3, p4 = quadrant_lines_from_box(box)
-    cut1, cut2 = opening_points(image_collection, series_id, box)
-
-    line = p2 - p1
-    m = line.magnitude
-    p1_inner = p1 + line * (cut1.x / line.magnitude)
-    p2_inner = p1 + line * (cut2.x / line.magnitude)
+    minor_pt1, minor_pt2 = minor_line(box)
+    inner_pt1, inner_pt2 = opening_line(image_collection, series_id, box)
 
 
 
@@ -51,8 +61,13 @@ def find_opening(image_collection, stomata_id, timepoint):
         fname = 'S{}_T{}_Z{}.png'.format(series_id, timepoint, z)
         im = np.dstack((proxy_im.image, proxy_im.image, proxy_im.image))
         cv2.ellipse(im, box, (0, 0, 255))
-        cv2.line(im, p1.astype("int").astuple(), p2.astype("int").astuple(), (0, 255, 0), 1)
-        cv2.line(im, p1_inner.astype("int").astuple(), p2_inner.astype("int").astuple(), (255, 0, 0), 1)
+        cv2.line(im,
+            minor_pt1.astype("int").astuple(),
+            minor_pt2.astype("int").astuple(),
+            (0, 255, 0), 1)
+        cv2.line(im,
+            inner_pt1.astype("int").astuple(),
+            inner_pt2.astype("int").astuple(), (255, 0, 0), 1)
         scipy.misc.imsave(fname, im)
 
 
